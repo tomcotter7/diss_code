487b89b51fd5ed96c7b7f5429fb9ab9ea552bc54
tcotts <thomascotter00@gmail.com>
2022-03-01 09:58:10 +0000

    added some example functions

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,11 @@
 def gradCAMplusplus():
+    # run gradCAM++ on an image
     pass
 
 def guidedBackProp():
+    # run guided back propagation to improve heatmap of an image
     pass
 
 def generateOutput():
+    # take an image, run model on it, run gradCAM++ on that 'lighted-up' model, produce output
     pass
