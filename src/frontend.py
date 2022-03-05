3a483658c807d764d8b8d835da29726ce2c3e2f1
tcotts <thomascotter00@gmail.com>
2022-03-05 16:23:43 +0000

    made a more class based structure for the front-end

--- src/frontend.py
+++ src/frontend.py
@@ -6,44 +6,47 @@ import time
 from tkinter.filedialog import askopenfile
 
 
-root = tk.Tk()
+class App:
+
+    def __init__(self):
+
+        self.root = tk.Tk()
+        self.root.title("Diabetic Retinopathy Detection")
+        self.image = ""
+
+        window_width = 700
+        window_height = 550
+        # get the screen dimension
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+
+        # find the center point
+        center_x = int(screen_width/2 - window_width / 2)
+        center_y = int(screen_height/2 - window_height / 2)
+
+        # set the position of the window to the center of the screen
+        self.root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+        file_upload = ttk.Button(self.root, text="Upload 2D Fundus Image",
+                                 command=lambda: self.upload_file())
+        file_upload.grid()
+
+        self.root.resizable(False, False)
+
+        # when loading a file in, root.lower()
+
+    def run(self):
+        self.root.mainloop()
+
+    def upload_file(self):
+        file_path = askopenfile(mode='r', filetypes=[('Image Files', '*jpeg')])
+        if file_path is not None:
+            pass
+        print("Selected:", file_path)
+        output = tk.Label(self.root, text=file_path.name)
+        output.grid()
+        self.image = file_path.name
 
 
-def upload_file():
-    file_path = askopenfile(mode='r', filetypes=[('Image Files', '*jpeg')])
-    if file_path is not None:
-        pass
-    print("Selected:", file_path)
-    output = tk.Label(root, text=file_path.name)
-    output.grid()
-
-
-def frontend():
-
-    root.title("Diabetic Retinopathy Detection")
-
-    window_width = 700
-    window_height = 550
-    # get the screen dimension
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    # find the center point
-    center_x = int(screen_width/2 - window_width / 2)
-    center_y = int(screen_height/2 - window_height / 2)
-
-    # set the position of the window to the center of the screen
-    root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-
-    file_upload = ttk.Button(root, text="Upload 2D Fundus Image",
-                             command=lambda: upload_file())
-    file_upload.grid()
-
-    root.resizable(False, False)
-
-    # when loading a file in, root.lower()
-
-    root.mainloop()
-
-
-frontend()
+new_app = App()
+new_app.run()
